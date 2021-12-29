0f09c1f4ebe7ed5bda506d3a9db0a62d0f735044
weiaijun <weiaijun@meicai.cn>
2021-12-29 12:00:43 +0800

    rustlings_anwser 4.6.0

---
+++ exercises/clippy/target/debug/deps/clippy2-7ec337aa5f90b8b5.d
@@ -0,0 +1,7 @@
+/Users/vimer/workSpace/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-7ec337aa5f90b8b5.rmeta: clippy2.rs
+
+/Users/vimer/workSpace/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-7ec337aa5f90b8b5.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
